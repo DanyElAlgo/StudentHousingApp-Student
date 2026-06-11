7a1b16aaf37192c94f1664237bb1ab5c6600aeb8
Daniel Roland Peñaranda Colque <eldanielitu@gmail.com>
2026-06-10 22:52:21 -0400

    input widget designs finished
    los widgets de campos de entrada están terminados y formarán parte del design system una vez todos los componentes estén listos

---
+++ lib/student_design_system/widgets/inputs/app_text_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatelessWidget {
+  const AppTextField({
+    super.key,
+    this.controller,
+    this.label,
+    this.hintText,
+    this.obscureText = false,
+    this.keyboardType,
+    this.textInputAction,
+    this.validator,
+    this.onChanged,
+    this.onFieldSubmitted,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.enabled = true,
+  });
+
+  final TextEditingController? controller;
+  final String? label;
+  final String? hintText;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final FormFieldValidator<String>? validator;
+  final ValueChanged<String>? onChanged;
+  final ValueChanged<String>? onFieldSubmitted;
+  final IconData? prefixIcon;
+  final Widget? suffixIcon;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      validator: validator,
+      onChanged: onChanged,
+      onFieldSubmitted: onFieldSubmitted,
+      enabled: enabled,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hintText,
+        prefixIcon: prefixIcon == null ? null : Icon(prefixIcon),
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
